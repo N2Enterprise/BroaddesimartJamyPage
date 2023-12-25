a60ff5e685dd9aeef222205cf78703569ab5d4ee
nomanpirwani <noman.pirwani@gmail.com>
2023-12-25 00:28:36 -0500

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   ViewTransitions: true,
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
