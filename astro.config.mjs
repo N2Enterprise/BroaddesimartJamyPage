2c5d34fa3f5a2eea5599f05bd918b3cce5470ff5
nomanpirwani <noman.pirwani@gmail.com>
2023-12-24 20:33:33 -0500

    cloud flare chnges

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   ViewTransitions: true,
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   integrations: [tailwind(), react()]
 });
